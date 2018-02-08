ae6540ec790b47e69928022f810572c0c58fbdc7
olanmj <olanm@stockton.edu>
2018-02-08 10:54:18 -0500

    Controllers and views with simple models

---
+++ DemoLab4/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoLab4.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            string greeting = "Hello from the Demo controller";
+            return View("Index", greeting);
+        }
+
+        public IActionResult Page2(int id)
+        {
+            return View(id);
+        }
+    }
+}
--- DemoLab4/Startup.cs
+++ DemoLab4/Startup.cs
@@ -43,7 +43,7 @@ namespace DemoLab4
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Demo}/{action=Index}/{id?}");
             });
         }
     }
